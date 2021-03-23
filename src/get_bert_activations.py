7a716fe9f2942bfb9a060963641bc6f19ef9c3cf
baom <baom@stanford.edu>
2021-03-22 20:25:50 -0700

    Get wikipedia data and activations

--- src/get_bert_activations.py
+++ src/get_bert_activations.py
@@ -1,90 +1,108 @@
 import torch
 import numpy as np
 import pickle as pkl
-from tqdm import tqdm
 from transformers import BertTokenizer, BertModel
 from nltk.tokenize import sent_tokenize
 from pathlib import Path
 import os
+import argparse
+import glob
 
 
-MAX_INPUT_SIZE = 512
-
-DIR = Path('../data/alice/sample3/')
-DOC_TO_READ = DIR / 'doc.txt'
-
-TOKENS_OUTPUT = DIR / 'tokens.pickle'
-ACTIVATIONS_OUTPUT = DIR / 'activations.npz'
-
-
-def tokenize_and_segment():
-    file = open(DOC_TO_READ)
-    lines = file.read().replace("\n", " ")
-    file.close()
+class GetEmbeddings:
     
-    sents = sent_tokenize(lines)
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
-    tokenized_texts = []
-    segments_ids = [0]
-
-    for i, sent in enumerate(sents):
-        marked_text = sent + " [SEP] "
-        tokenized_text = tokenizer.tokenize(marked_text)
-        tokenized_texts.extend(tokenized_text)
-        segments_ids.extend([i] * len(tokenized_text))
-        
-    # Clip tokens to MAX_INPUT_SIZE
-    tokenized_texts, segments_ids = tokenized_texts[:MAX_INPUT_SIZE - 1], segments_ids[:MAX_INPUT_SIZE - 1]
-    tokenized_texts =  ["[CLS]"] + tokenized_texts
-    segments_ids = [segments_ids[0]] + segments_ids
-    
-    # Pickle the tokenized text
-    f = open(TOKENS_OUTPUT, 'wb')
-    pkl.dump(tokenized_texts, f)
-    
-    tokens_tensors = []
-    segments_tensors = []
-
-    # Map the token strings to their vocabulary indeces.
-    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_texts)
-
-    # Convert inputs to PyTorch tensors
-    tokens_tensor = torch.tensor([indexed_tokens])
-    segments_tensor = torch.tensor([segments_ids])
-
-    return tokens_tensor, segments_tensor
-
-
-def extract_embeddings(tokens_tensor, segments_tensor):
-    model = BertModel.from_pretrained('bert-base-uncased',
+    def __init__(self, bert_type='bert-base-cased'): 
+        self.bert_type = bert_type
+        self.tokenizer = BertTokenizer.from_pretrained(self.bert_type)
+        self.model = BertModel.from_pretrained(self.bert_type,
                                       output_hidden_states = True, # Whether the model returns all hidden-states.
                                       )
-    # Put the model in "evaluation" mode, meaning feed-forward operation.
-    model.eval()
+        # Put the model in "evaluation" mode, meaning feed-forward operation.
+        self.model.eval()
+
+        # TODO: explore custom BERT config to increase input size
+        # Note that input files will be clipped to this many tokens
+        self.MAX_INPUT_SIZE = 512 # default maximum sequence length for BERT
+
+
+    def tokenize_and_extract(self, txt_file):
+
+        file = open(txt_file)
+        lines = file.read()
+        file.close()
+
+        sentences = sent_tokenize(lines)
+
+        tokenized_texts = []
+        # segment_ids are tokens used to differentiate between different sentences.
+        segment_ids = []
+
+        for i, sent in enumerate(sentences):
+            marked_text = sent + " [SEP] "
+            tokenized_text = self.tokenizer.tokenize(marked_text)
+            tokenized_texts.extend(tokenized_text)
+            segment_ids.extend([i] * len(tokenized_text))
+
+        for i in range(0, len(tokenized_texts), self.MAX_INPUT_SIZE):
+            start = i
+            end = i + self.MAX_INPUT_SIZE
+
+            tokenized_texts_subset, segment_ids_subset = tokenized_texts[start:end - 1], segment_ids[start:end - 1]
+            tokenized_texts_subset =  ["[CLS]"] + tokenized_texts_subset
+            segment_ids_subset = [segment_ids_subset[0]] + segment_ids_subset
+            
+            data_dir = txt_file[:txt_file.rindex('/')]
+            tokens_file = f'{data_dir}/tokens_{i % self.MAX_INPUT_SIZE}_{self.bert_type}.pickle'
+
+            # Pickle the tokenized text
+            f = open(tokens_file, 'wb')
+            pkl.dump(tokenized_texts_subset, f)
+
+            # Map the token strings to their vocabulary indeces.
+            indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_texts_subset)
+
+            # Convert inputs to PyTorch tensors
+            token_tensor = torch.tensor([indexed_tokens])
+            segment_tensor = torch.tensor([segment_ids_subset])
+
+            with torch.no_grad():
+                outputs = self.model(token_tensor, segment_tensor)
+
+                # Evaluating the model will return a different number of objects based on 
+                # how it's  configured in the `from_pretrained` call earlier. In this case, 
+                # becase we set `output_hidden_states = True`, the third item will be the 
+                # hidden states from all layers. See the documentation for more details:
+                # https://huggingface.co/transformers/model_doc/bert.html#bertmodel
+                hidden_states = outputs[2]
+
+                # Concatenate the tensors for all layers. We use `stack` here to
+                # create a new dimension in the tensor.
+                token_embeddings = torch.stack(hidden_states, dim=0)
+
+                # Remove dimension 1, the "batches".
+                token_embeddings = torch.squeeze(token_embeddings, dim=1)
+
+                # token_embeddings is now of shape (13, 512, 768) aka (# layers, # tokens, width of layer)
+                arr = np.array(token_embeddings)
+                activations_file = f'{data_dir}/activations_{i % self.MAX_INPUT_SIZE}_{self.bert_type}.npz'
+                np.savez(activations_file, *arr)
+
+
+# Run like so:  python get_bert_activations.py -d ~/bert-vis/data/wikipedia/ -b 'bert-base-cased'
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--dir", help = "directory to recursively extract embeddings for")
+parser.add_argument("-b", "--bert", help = "probably want bert-base-cased or bert-large-cased")
     
-    with torch.no_grad():
-        outputs = model(tokens_tensor, segments_tensor)
+# Read arguments from command line
+args = parser.parse_args()
 
-        # Evaluating the model will return a different number of objects based on 
-        # how it's  configured in the `from_pretrained` call earlier. In this case, 
-        # becase we set `output_hidden_states = True`, the third item will be the 
-        # hidden states from all layers. See the documentation for more details:
-        # https://huggingface.co/transformers/model_doc/bert.html#bertmodel
-        hidden_states = outputs[2]
+Embeddings = GetEmbeddings()
 
-        # Concatenate the tensors for all layers. We use `stack` here to
-        # create a new dimension in the tensor.
-        token_embeddings = torch.stack(hidden_states, dim=0)
+print("data dir: ", args.dir)
 
-        # Remove dimension 1, the "batches".
-        token_embeddings = torch.squeeze(token_embeddings, dim=1)
+for txt_file in glob.iglob(args.dir + '**/*.txt', recursive=True):
+    print("txt: ", txt_file)
+    Embeddings.tokenize_and_extract(txt_file)
 
-        # token_embeddings is now of shape (13, 512, 768) aka (# layers, # tokens, width of layer)
-
-        arr = np.array(token_embeddings)
-        np.savez(ACTIVATIONS_OUTPUT, *arr)
-        
-
-tokens_tensor, segments_tensor = tokenize_and_segment()
-extract_embeddings(tokens_tensor, segments_tensor)
+    
