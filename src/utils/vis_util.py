3f91e755feadafbaab454c18187383380077cc86
Pratyusha Kalluri <pkalluri@stanford.edu>
2021-04-13 13:12:21 -0700

    Clean up visualization and improve efficiency.

--- src/utils/vis_util.py
+++ src/utils/vis_util.py
@@ -1,6 +1,13 @@
 import math
 import numpy as np
+from bokeh.palettes import Inferno, Category10, Category20, Category20c, Pastel1, Pastel2, Bokeh, Plasma, Colorblind
+from bokeh.plotting import figure, show, output_file
+from bokeh.layouts import gridplot, column
+from bokeh.models import Div, HoverTool, ColumnDataSource, PanTool, BoxZoomTool, WheelZoomTool, ResetTool
 from bokeh.palettes import Inferno, Category10, Category20, Category20c, Pastel1, Pastel2, Bokeh, Plasma
+from bokeh.models.annotations import Legend, LegendItem
+import math
+from src.utils import vis_util
 
 
 def hue_to_rgb(ang, warp=True):
@@ -59,8 +66,8 @@ def channels_to_rgbs(X):
     return rgbs.astype(int)
 
 
-DEFAULT_PALETTE = Bokeh[8][::2]
-BIG_PALETTE = Plasma[256]
+DEFAULT_PALETTE = Colorblind[8][:]
+BIG_PALETTE = Plasma[256][:230]
 
 
 def categorical_list_to_color_list(categorical_list, palette=BIG_PALETTE, reverse_palette=False):
@@ -71,3 +78,147 @@ def categorical_list_to_color_list(categorical_list, palette=BIG_PALETTE, revers
     sampled_palette = palette[::n_colors_per_category] + (palette[-1],)
     categories_to_colors = {category: sampled_palette[i] for i, category in enumerate(categories)}
     return [categories_to_colors[elt] for elt in categorical_list]
+
+
+def empty_plot(dim=250, darkmode=False):
+    p = figure(width=dim, height=dim)
+    p.axis.visible = False
+    p.grid.visible = False
+    if darkmode:
+        p.background_fill_color = 'lightgrey'
+    return p
+
+
+def custom_hover_tool(label='{context html}'):
+    hover_tool = HoverTool(
+        tooltips=
+        f"""<div style=
+            '
+            border-bottom-style:solid;
+            border-width:1px;
+            '
+            >@{label}</div>"""
+    )
+    return hover_tool
+
+
+def plot_plain(contexts, layer, acts_tag, legend=False, darkmode=True):
+    """
+    Given a layer, and a list of binary columns in the contexts dataframe,
+    plots all activations at the specified layer, colorized to visualize all the specified columns.
+    TODO: document requirements of contexts dataframe.
+    """
+    p = empty_plot(darkmode=darkmode)
+    if legend:  # add legend
+        p.height += 200
+        p.add_layout(Legend(orientation='vertical', label_text_font_size='6pt', label_width=10), 'above')
+    # add all contexts in grey
+    source = ColumnDataSource(
+        {
+            'x': contexts[f'{layer} {acts_tag} x'],
+            'y': contexts[f'{layer} {acts_tag} y'],
+            'token': contexts['token'],
+            'abbreviated context': contexts['abbreviated context'],
+            'abbreviated context html': contexts['abbreviated context html'],
+            'context html': contexts['context html']
+        }
+    )
+    p.circle('x', 'y', color='black', source=source)
+    p.tools = [PanTool(), WheelZoomTool(), BoxZoomTool(), ResetTool(), custom_hover_tool()]
+    return p
+
+
+def plot_binary_columns(contexts, layer, acts_tag, color_cols, legend=False):
+    """
+    Given a layer, and a list of binary columns in the contexts dataframe,
+    plots all activations at the specified layer, colorized to visualize all the specified columns.
+    TODO: document requirements of contexts dataframe.
+    """
+    p = empty_plot()
+    if legend:  # add legend
+        p.height += 200
+        p.add_layout(Legend(orientation='vertical', label_text_font_size='6pt', label_width=10), 'above')
+
+    # add all contexts in grey
+    source = ColumnDataSource(
+        {
+            'x': contexts[f'{layer} {acts_tag} x'],
+            'y': contexts[f'{layer} {acts_tag} y'],
+            'token': contexts['token'],
+            'abbreviated context': contexts['abbreviated context'],
+            'abbreviated context html': contexts['abbreviated context html'],
+            'context html': contexts['context html']
+        }
+    )
+    p.circle('x', 'y', color='lightgrey', source=source)
+    # add each column's contexts in a color
+    for col_idx, col in enumerate(color_cols):
+        selected_contexts = contexts[contexts[col]]
+        source = ColumnDataSource(
+            {
+                'x': selected_contexts[f'{layer} {acts_tag} x'],
+                'y': selected_contexts[f'{layer} {acts_tag} y'],
+                'token': selected_contexts['token'],
+                'abbreviated context': selected_contexts['abbreviated context'],
+                'abbreviated context html': selected_contexts['abbreviated context html'],
+                'context html': selected_contexts['context html'],
+            }
+        )
+        if legend:
+            p.circle('x', 'y', color=vis_util.DEFAULT_PALETTE[col_idx], legend_label=col, source=source)
+        else:
+            p.circle('x', 'y', color=vis_util.DEFAULT_PALETTE[col_idx], source=source)
+
+    p.tools = [PanTool(), WheelZoomTool(), BoxZoomTool(), ResetTool(), custom_hover_tool()]
+    return p
+
+
+def plot_categorical_column(contexts, layer, acts_tag, color_col, legend=False):
+    """
+    Given a layer and a categorical column in the contexts dataframe,
+    plots all activations at the specified layer, colorized to visualize the specified column.
+    TODO: document requirements of contexts dataframe.
+    """
+    p = empty_plot()
+    if legend:  # add legend
+        p.height += 200
+        p.add_layout(Legend(orientation='horizontal', label_text_font_size='6pt', label_width=10), 'above')
+
+    source = ColumnDataSource(
+        {
+            'x': contexts[f'{layer} {acts_tag} x'],
+            'y': contexts[f'{layer} {acts_tag} y'],
+            'color': vis_util.categorical_list_to_color_list(contexts[color_col]),
+            'legend label': contexts[color_col],
+            'token': contexts['token'],
+            'abbreviated context': contexts['abbreviated context'],
+            'abbreviated context html': contexts['abbreviated context html'],
+            'context html': contexts['context html']
+        }
+    )
+    if legend:
+        p.circle('x', 'y', color='color', legend_group='legend label', source=source)
+    else:
+        p.circle('x', 'y', color='color', source=source)
+
+    p.tools = [PanTool(), WheelZoomTool(), BoxZoomTool(), ResetTool(), custom_hover_tool()]
+    return p
+
+
+def visualize_columns(contexts, layers, acts_tag, color_cols):
+    """
+    Given a list of layers, creates a plot of each layer,
+    colorized to depict the info in the specified columns in the contexts dataframe.
+    TODO: document requirements of contexts dataframe.
+    """
+    if len(color_cols) == 0:
+        title = Div(text=f'{acts_tag}<br>' + ' / '.join(color_cols), align='center')
+        return [title] + [plot_plain(contexts, layer, acts_tag, legend=(i == 0)) for i, layer in enumerate(layers)]
+    elif len(color_cols) == 1:
+        title = Div(text=f'{acts_tag}<br>' + ' / '.join(color_cols), align='center')
+        return [title] + [plot_categorical_column(contexts, layer, acts_tag, color_cols[0], legend=(i == 0))
+                          for i, layer in enumerate(layers)]
+    else:
+        title = Div(text=f'{acts_tag}', align='center')
+        return [title] + [plot_binary_columns(contexts, layer, acts_tag, color_cols, legend=(i == 0))
+                          for i, layer in enumerate(layers)]
