51aa5563ae7a1f09b54531e4f5cfc60554ef7584
Pratyusha Ria Kalluri <pkalluri@stanford.edu>
2021-04-13 13:21:52 -0700

    Clean up+speed up wiki visualization and add script to get sentences' Nearest Neighbors. (#6)
    * Modularize + clean up visualization + improve efficiency
    
    * Script to get sentences' nearest neighbors at every layer

--- src/utils/context_util.py
+++ src/utils/context_util.py
@@ -1,18 +1,34 @@
 """Helpers for handling contexts. Contexts are typically tuples (doc's tokens, index of embedded token)"""
+from src.utils import html_util
 
 
-def context_str(toks, tok_i):
+def bracket(s):
+    return f'[[{s}]]'
+
+
+def context_str(toks, tok_pos, marker=bracket):
     """Get a string representation of the context: the doc with emphasis on the embedded token."""
-    return ' '.join([tok if i != tok_i else f'[[{tok}]]' for (i, tok) in enumerate(toks)])
+    s = ''
+    for i, tok in enumerate(toks):
+        cleaned_tok: str = tok
+        if cleaned_tok.startswith('##'):
+            cleaned_tok = f'/{tok[2:]}'
+            if i == tok_pos: cleaned_tok = marker(cleaned_tok)
+        else:
+            if i == tok_pos: cleaned_tok = marker(cleaned_tok)
+            cleaned_tok = ' ' + cleaned_tok
+        s += cleaned_tok
+    return s
 
 
-def emphasized_html(s):
-    return f'<b style="color: hotpink">{s}</b>'
+def context_plaintext(toks, tok_pos):
+    """Get a string representation of the context: the doc with emphasis on the embedded token."""
+    return context_str(toks, tok_pos, bracket)
 
 
 def context_html(toks, tok_pos):
     """Get a string representation of the context: the doc with emphasis on the embedded token."""
-    return ' '.join([tok if i != tok_pos else f' {emphasized_html(tok)} ' for (i, tok) in enumerate(toks)])
+    return context_str(toks, tok_pos, html_util.highlight_html)
 
 
 def abbreviated_context_html(toks, tok_pos, n_context_tokens=2):
@@ -24,7 +40,7 @@ def abbreviated_context_html(toks, tok_pos, n_context_tokens=2):
         new_tok_pos = n_context_tokens
     else:
         # we do not have a complete abbreviated context;
-        # abbreviated context will start at beginning of tokens
+        # abbreviated context will start at context's first token
         start_index = 0
         new_tok_pos = tok_pos
     end_index = min(tok_pos + n_context_tokens + 1, len(toks))
---
+++ src/utils/html_util.py
@@ -0,0 +1,12 @@
+
+
+def color_html(s, color='hotpink'):
+    return f'<span style="color: {color}">{s}</span>'
+
+
+def highlight_html(s, color='lightpink'):
+    return f'<span style="background-color: {color}">{s}</span>'
+
+
+def bold_html(s):
+    return f'<b>{s}</b>'
