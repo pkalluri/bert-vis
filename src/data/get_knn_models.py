fc476ef1dd689c180956e28771469737ed1a34dd
Pratyusha <pkalluri@stanford.edu>
2021-09-10 14:38:06 -0700

    Bugfix: For users who wish to analyze only a subset of layers, knn models should be saved and loaded in a way that makes this possible.

--- src/data/get_knn_models.py
+++ src/data/get_knn_models.py
@@ -1,45 +1,39 @@
-"""Given a data directory containing an acts file (usually a large corpus), creates a pickle file containing,
-in order, every layer's KNN model. These can be used downstream to get any vector's evolution of KNNs."""
+'''Given the path to a directory containing an acts file,
+creates a subdirectory containing the corresponding knn models
+(in the form of pickle files names '[layer name].pickle').'''
 
-import os
 import pickle
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
-import argparse
+import click
 import random
 random.seed(0)
+
+import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))  # add CWD to path
 from src.utils import references as refs
 from src.utils import acts_util
 
 
-# Args
-argparser = argparse.ArgumentParser()
-argparser.add_argument('data_path', help='Data directory containing an activations file.'
-                                         'The generated KNN file will also be put here.')
-argparser.add_argument('-a', '--algorithm', default='auto', help='which KNN algorithm.')
-argparser.add_argument('-n', '--nneighbors', default=50, help='Set default number of neighbors to get when queried.')
-argparser.add_argument('-o', '--output', default='', help='name of output file')
-argparser.add_argument('-s', '--spherize', default=False, action='store_true', help='spherize acts to unit norm first?')
-args = argparser.parse_args()
+@click.command(help='Given the path to a directory containing an acts file, ' \
+    'creates a knn models subdirectory containing pickled knn models.',)
+@click.argument('data_path')
+def make_knn_models(data_path):
+    data_path = os.path.abspath(data_path)
+    acts_path = os.path.join(data_path, refs.acts_fn)
+    models_path = os.path.join(data_path, refs.knn_models_dirname)
+    os.mkdir(models_path)
 
-
-# Prepping filenames
-data_path = os.path.abspath(args.data_path)
-acts_fn = refs.acts_fn
-knn_models_fn = args.output if args.output else refs.knn_models_fn
-
-# Build and save nearest neighbor models - for facilitating fast nearest neighbor search
-acts = np.load(os.path.join(data_path, acts_fn))
-with open(os.path.join(data_path, knn_models_fn), 'wb') as knn_models_file:
-    for layer in acts.files:
+    acts = np.load(acts_path)
+    model = NearestNeighbors()
+    for layer in acts:
+        print(f'Fitting layer {layer}')
         _acts = acts[layer]
-        if args.spherize:
-            print('Spherizing')
-            _acts = acts_util.spherize(_acts)
-        print(f'Modeling {layer}')
-        knn_model = NearestNeighbors(algorithm=args.algorithm, n_neighbors=args.nneighbors).fit(_acts)
-        print('Writing')
-        pickle.dump(knn_model, knn_models_file)
-        del knn_model
+        model.fit(_acts)
+        with open(os.path.join(models_path, f'{layer}.pickle'), 'wb') as f:
+            pickle.dump(model, f)
+
+
+if __name__ == '__main__':
+    make_knn_models()
