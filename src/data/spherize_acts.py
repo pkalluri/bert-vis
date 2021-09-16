c9ef9111b2e2bf5723f89e9193c5182636de3d94
Pratyusha <pkalluri@stanford.edu>
2021-09-16 02:02:49 -0700

    Minimized required preprocessing, so simplified usage of Building Blocks notebook

--- src/data/spherize_acts.py
+++ src/data/spherize_acts.py
@@ -7,8 +7,8 @@ from src.utils import references as refs
 from src.utils.acts_util import spherize
 
 parser = argparse.ArgumentParser()
-parser.add_argument('data_dir', help='Path to directory containing input acts file.')
-parser.add_argument('out_data_dir', help='Path to directory to put output spherized acts file.')
+parser.add_argument('data_dir', help='Directory containing input standard acts file.')
+parser.add_argument('out_data_dir', help='Directory in which to put output spherized acts file.')
 args = parser.parse_args()
 
 acts = np.load(os.path.join(args.data_dir, refs.acts_fn))
